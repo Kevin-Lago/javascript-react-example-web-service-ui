f0353ef8f095a4fdc5f5a69d0237515750f7d8d7
Kevin Lago <kevinthelago@gmail.com>
2023-05-20 10:43:56 -0400

    Testing Login

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { getCurrentUser } from './util/APIUtil';
 import { ACCESS_TOKEN } from './constants';
 import './App.css';
-import SciFiScreen from './login/SciFiScreen';
+// import SciFiScreen from './login/SciFiScreen';
 import Home from './home/Home';
 import PrivateRoute from './util/PrivateRoute';
+import Login from './login/Login';
+import OAuth2RedirectHandler from './oauth2/OAuth2RedirectHandler';
+import NotFound from './notfound/NotFound';
+import Profile from './profile/Profile';
 
 export default class App extends Component {
     constructor(props) {
@@ -52,7 +56,10 @@ export default class App extends Component {
                     <Routes>
                         <Route path="/" element={<PrivateRoute><Home authenticated={this.state.authenticated} currentUser={this.state.currentUser} /></PrivateRoute>} />
                         {/* <PrivateRoute path="/" element={<Home />} authenticated={this.state.authenticated} currentUser={this.state.currentUser} /> */}
-                        <Route path="/login" element={<SciFiScreen title="Login" screenStack={6} />} />
+                        <Route path="/login" element={<Login authenticated={this.state.authenticated} {...this.props} />} />
+                        <Route path="/profile" element={<PrivateRoute path="/profile" authenticated={this.state.authenticated} currentUser={this.state.currentUser} component={<Profile />} />} />
+                        <Route path="/oauth2/redirect" component={OAuth2RedirectHandler}></Route>
+                        <Route component={NotFound}></Route>
                     </Routes>
                 </BrowserRouter>
             </div>
