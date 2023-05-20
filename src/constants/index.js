868d74579498e8401c6854b7bae5d458a3caf823
Kevin Lago <kevinthelago@gmail.com>
2023-05-20 13:23:38 -0400

    GitHub Callback Test

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,4 +4,4 @@ export const API_BASE_URL = 'http://localhost:8080';
 export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/callback'
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorization/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const GITHUB_AUTH_URL = API_BASE_URL + '/oauth2/authorization/github?redirect_uri=' + OAUTH2_REDIRECT_URI;
+export const GITHUB_AUTH_URL = API_BASE_URL + '/oauth2/authorization/github?redirect_uri=' + OAUTH2_REDIRECT_URI + '/github';
